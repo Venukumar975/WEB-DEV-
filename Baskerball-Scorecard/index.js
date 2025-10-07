be308667105b6241c4c303a1da39d0db53635504
venu <pichikavenu975@gmail.com>
2025-10-07 12:53:39 +0530

    basketball scorecard

---
+++ Baskerball-Scorecard/index.js
@@ -0,0 +1,28 @@
+let Score1El = 0
+let Score2El = 0
+let Score1 = document.getElementById("score1")
+let Score2 = document.getElementById("score2")
+function Add1() {
+    Score1El += 1
+    Score1.innerText = Score1El
+}
+function Add2() {
+    Score1El += 2
+    Score1.innerText = Score1El
+}
+function Add3() {
+    Score1El += 3
+    Score1.innerText = Score1El
+}
+function _Add1() {
+    Score2El += 1
+    Score2.innerText = Score2El
+}
+function _Add2() {
+    Score2El += 2
+    Score2.innerText = Score2El
+}
+function _Add3() {
+    Score2El += 3
+    Score2.innerText = Score2El
+}
