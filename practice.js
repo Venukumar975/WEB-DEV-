e494a7665840db3d0a00145267449913f3706268
venu <pichikavenu975@gmail.com>
2025-10-16 20:15:35 +0530

    added manifest.json

---
+++ practice.js
@@ -0,0 +1,11 @@
+const recipent = "james"
+let name = "venu"
+// Refactor the email string to use template string
+
+const email = `hey 
+${recipent} 
+How is it going? ${name}`
+
+console.log(email)
+
+
