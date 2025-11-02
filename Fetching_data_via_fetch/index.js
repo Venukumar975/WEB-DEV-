885f23e50d86505fc9fcac6d8a78cb9b00177aea
venu <pichikavenu975@gmail.com>
2025-11-02 15:39:30 +0530

    created a fetch api

---
+++ Fetching_data_via_fetch/index.js
@@ -0,0 +1,28 @@
+
+async function fetchData() {
+    try{
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+    if (!response.ok){
+      throw new Error("could not fetch the data");
+
+    }
+    const data = await response.json();
+    const pokemonImage = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonImage");
+
+    imgElement.src = pokemonImage;
+    imgElement.style.display = "block";
+  
+
+    }
+    catch(error){
+
+    }
+    
+}
+
+
+
+
