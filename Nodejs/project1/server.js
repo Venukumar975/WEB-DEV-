4a437c2ccfbbb567111535adeb941c3272de8500
venu <pichikavenu975@gmail.com>
2025-11-07 02:26:44 +0530

    created an response for an api request from the client

--- Nodejs/project1/server.js
+++ Nodejs/project1/server.js
@@ -1,12 +1,16 @@
 import http from 'node:http'
 
-
-const PORT = 5000
-const server = http.createServer((request , response) => {
-  response.end("Hello from the server")
+const PORT = 6000
+const server = http.createServer((req,res)=>{
+  if (req.url === '/api'){
+    res.end('Here is your data from server')
+  }
+  else{
+    res.end('Hello from the server')
+  }
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+server.listen(PORT,()=>{
+    console.log('Server is running on port '+ PORT)
+    console.log('http://localhost:'+PORT)
 })
-
