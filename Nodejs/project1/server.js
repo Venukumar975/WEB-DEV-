92ea56bd56f176c02e6ab9b4603788ef90e210dd
venu <pichikavenu975@gmail.com>
2025-11-08 00:50:37 +0530

    Completed the simple rest api project it is named as proejct1

--- Nodejs/project1/server.js
+++ Nodejs/project1/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 import { getDataFromDB } from './database.js'
 import { sendJSONResponse } from './utility/sendJSONResponse.js'
 import { getFilteredData } from './utility/getFileterdData.js'
+import { getDataByQueryParams } from './utility/getDataByQueryParams.js'
 
 const PORT = 6000
 
@@ -10,25 +11,26 @@ const server = http.createServer(async (req,res)=>{
   const urlObj = new URL(req.url, `http://${req.headers.host}` )
   const queryObj = Object.fromEntries(urlObj.searchParams)
   if (urlObj.pathname === '/api' && req.method === 'GET'){
-    sendJSONResponse(res,200,destinations)
+    let filteredData = getDataByQueryParams(destinations, queryObj)
+    sendJSONResponse(res,200,filteredData)
     console.log(queryObj)
   }
   else if (req.url.startsWith('/api/continent') && req.method === 'GET'){
      const continent = req.url.split('/').pop()
-     const filterData = getFilteredData(destinations , 'continent', continent)
+     const filteredData = getFilteredData(destinations , 'continent', continent)
       
-    sendJSONResponse(res,200,filterData)
+    sendJSONResponse(res,200,filteredData)
 
   }
    else if (req.url.startsWith('/api/country') && req.method === 'GET'){
      const country = req.url.split('/').pop()
-     const filterData = getFilteredData(destinations,'country',country)
+     const filteredData = getFilteredData(destinations,'country',country)
      
-    sendJSONResponse(res,200,filterData)
+    sendJSONResponse(res,200,filteredData)
 
   }
   else{
-    
+    res.setHeader('Content-Type','application/json')
     sendJSONResponse(res,404,{
       error : "route not found",
       message : "The requested route does not exist"
