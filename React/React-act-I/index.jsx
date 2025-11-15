688a59b83f95f4ce9f7adda9507c56f41936eaf8
venu <pichikavenu975@gmail.com>
2025-11-15 14:32:15 +0530

    Did a small react static project

--- React/React-act-I/index.jsx
+++ React/React-act-I/index.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 
-export default function Myfun(){
+export default function WelcomeFun(){
     return (
         <div>
             <h1> Hello </h1>
             <p> Welcome to React </p>
-            <img src="react-logo.webp" alt="React-Logo" />
         </div>
         
     )
 }
 
+export function LoadReactIcon(){
+    return (
+        <img src = "react-logo-1.jpg" alt = 'React-Logo'/>
+    )
+}
--- React/React-act-I/main.jsx
+++ React/React-act-I/main.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
-import Myfun from "./index.jsx"
+import WelcomeFun , { LoadReactIcon as LoadImg } from "./index.jsx"
 
 const root = createRoot(document.getElementById("root"))
 
 // In react when we want to return multiple elements we should wrap them up in a parent element like div ( this is called random housekeeping )
 root.render(
-    <div>
-        <Myfun/>
-        <Myfun/>
+    <main>
+        <WelcomeFun/>
+
+        <LoadImg/>
+        <h1>
+           Fun facts about <span />React!
+        </h1>
+        <ul>
+            <li>First release in 2013</li>
+            <li>Was originally created by Jordan Walker</li>
+            <li>Has well over 200k stars on GitHub</li>
+            <li>Is mainted by Meta</li>
+            <li>Powers , thousands of enterprise apps , including mobile apps</li>
+        </ul>
         
 
-    </div>
+    </main>
     
 )
