4b82f9395718b968bcf5958e221c38e638226f0d
venu <pichikavenu975@gmail.com>
2025-11-15 12:55:30 +0530

    Working with react

---
+++ React/React-act-I/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Myfun(){
+    return (
+        <div>
+            <h1> Hello </h1>
+            <p> Welcome to React </p>
+        </div>
+        
+    )
+}
+
