586cf55b988503765fb1f3152e99b5b89c9b0aa3
venu <pichikavenu975@gmail.com>
2025-11-15 13:51:30 +0530

    Showed reusability and house-keeping concept in react

--- React/React-act-I/index.jsx
+++ React/React-act-I/index.jsx
@@ -5,6 +5,7 @@ export default function Myfun(){
         <div>
             <h1> Hello </h1>
             <p> Welcome to React </p>
+            <img src="react-logo.webp" alt="React-Logo" />
         </div>
         
     )
--- React/React-act-I/main.jsx
+++ React/React-act-I/main.jsx
@@ -4,6 +4,13 @@ import Myfun from "./index.jsx"
 
 const root = createRoot(document.getElementById("root"))
 
+// In react when we want to return multiple elements we should wrap them up in a parent element like div ( this is called random housekeeping )
 root.render(
-    <Myfun/>
+    <div>
+        <Myfun/>
+        <Myfun/>
+        
+
+    </div>
+    
 )
